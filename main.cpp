1bb615629a5389cd56510b84a33b83da6fa2175f
Pal Arpad <arpika@gmail.com>
2020-09-05 09:54:23 +0000

    Final Version of Gen weight

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ using namespace std;
 int main() {
 double g;
   int h, v;
+  char gen;
   cout << "inaltimea (cm) = "; cin >> h;
-  cout << "varsta (ani) = "; cin >> v; 
+  cout << "varsta (ani) = "; cin >> v;
+  cout << "gen (b, f) = "; cin >> gen;
   g = 50 + 3./4. * (h-150) + 1./4. * (v-20);
-  cout << "Greutatea ideala: barbat: " << g << "kg, femeie: " << g * 0.9 << "kg" << endl;
+  if (gen == 'b')
+  cout << "Greutatea ideala: barbat: " << g << "kg";
+  else
+  cout << "Greutatea ideala femeie " << g * 0.9 << "kg" << endl;
 }
